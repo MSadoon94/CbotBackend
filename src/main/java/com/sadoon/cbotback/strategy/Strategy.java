733f6fb607ba92bd8c5dd93e1aa21d8d5a97205d
Msadoon94 <M.sadoon94@gmail.com>
2021-12-26 14:40:01 -0700

    Add base and quote fields to strategy object

--- src/main/java/com/sadoon/cbotback/strategy/Strategy.java
+++ src/main/java/com/sadoon/cbotback/strategy/Strategy.java
@@ -2,6 +2,8 @@ package com.sadoon.cbotback.strategy;
 
 public class Strategy {
     private String name;
+    private String base;
+    private String quote;
 
     public void setName(String name) {
         this.name = name;
@@ -9,5 +11,21 @@ public class Strategy {
 
     public String getName() {
         return name;
+    }
+
+    public String getBase() {
+        return base;
+    }
+
+    public void setBase(String base) {
+        this.base = base;
+    }
+
+    public String getQuote() {
+        return quote;
+    }
+
+    public void setQuote(String quote) {
+        this.quote = quote;
     }
 }
--- src/main/java/com/sadoon/cbotback/strategy/StrategyRepository.java
+++
@@ -1,8 +0,0 @@
-package com.sadoon.cbotback.strategy;
-
-import com.mongodb.lang.NonNull;
-import org.springframework.data.mongodb.repository.MongoRepository;
-
-public interface StrategyRepository extends MongoRepository<Strategy, String> {
-    Strategy getStrategyByName(@NonNull String name);
-}
